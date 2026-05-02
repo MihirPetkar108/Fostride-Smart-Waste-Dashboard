557e80aaa7119f34486fd18a56750273b6a392b1
MihirPetkar108 <mihirpetkar2006@gmail.com>
2026-05-02 14:10:49 +0530

    Checking PORT

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ import adminRouter from "./routes/admin.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
+console.log(`Backend running on Port ${PORT}`);
 
 app.use(express.json());
 app.use(
